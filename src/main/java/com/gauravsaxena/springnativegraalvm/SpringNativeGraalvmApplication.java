d902593923a7ca87d7e32e81cc8dc01960463325
Gaurav Saxena <gauravsaxena.ec@gmail.com>
2022-03-29 21:58:34 +0200

    Initial commit

---
+++ src/main/java/com/gauravsaxena/springnativegraalvm/SpringNativeGraalvmApplication.java
@@ -0,0 +1,26 @@
+package com.gauravsaxena.springnativegraalvm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+import static org.springframework.web.reactive.function.server.RouterFunctions.route;
+import static org.springframework.web.reactive.function.server.ServerResponse.ok;
+
+@SpringBootApplication
+public class SpringNativeGraalvmApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringNativeGraalvmApplication.class, args);
+	}
+
+	@Bean
+	RouterFunction<ServerResponse> routes() {
+		return route()
+				.GET("/", request -> ok().body(Mono.just("Spring Native and Beyond!"), String.class))
+				.build();
+	}
+}
---
+++ src/test/java/com/gauravsaxena/springnativegraalvm/SpringNativeGraalvmApplicationTests.java
@@ -0,0 +1,26 @@
+package com.gauravsaxena.springnativegraalvm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.AutoConfigureWebTestClient;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@AutoConfigureWebTestClient
+class SpringNativeGraalvmApplicationTests {
+
+	@Autowired
+	private WebTestClient webClient;
+
+
+	@Test
+	void contextLoads() {
+		webClient
+				.get().uri("/")
+				.exchange()
+				.expectStatus().is2xxSuccessful()
+				.expectBody(String.class).isEqualTo("Spring Native and Beyond!");
+	}
+
+}
